d5384d9e6243530eedbe46e5b6f67557b414e499
Daniel <domarenon@gmail.com>
2017-02-17 22:15:57 -0500

    no message

---
+++ PeClustal/src/peclustal/PeClustal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peclustal;
+
+/**
+ *
+ * @author DANIEL OROZCO
+ */
+public class PeClustal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        peclustal.vistas.Principalm a = new peclustal.vistas.Principalm();
+        a.setVisible(true);
+        a.setLocationRelativeTo(null); 
+                
+    }
+    
+}
